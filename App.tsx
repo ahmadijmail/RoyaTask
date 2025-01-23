2808975e132bfa081ec82c009752c31856c513a7
ahmadijmail <ahmadijmil2010@gmail.com>
2025-01-24 00:54:12 +0300

    feat: implement basic video player

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React, {useRef} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {VideoPlayer} from './src/components/VideoPlayer';
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+
+      <VideoPlayer
+        source="https://roya-vod.ercdn.net/hls/i/r0/ixev5jcihldq43huwlykbswxmt0qv1qcaojfyx8h/ixev5jcihldq43huwlykbswxmt0qv1qcaojfyx8h_600.mp4"
+        title="Family Matter"
+        seasonNumber={3}
+        episodeNumber={1}
+        hasAds={true}
+        nextEpisode={() => {
+          // Handle next episode
+        }}
+      />
     </View>
   );
 }
